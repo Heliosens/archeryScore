cad6eb10fd572f699307a3759c1c367eb226efe9
Sylvie-B <unsolo@hotmail.fr>
2022-01-24 16:37:21 +0100

    create parts

---
+++ partials/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Séance de tir</title>
+    <link rel="stylesheet" href="/assets/style.css">
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/img/minTarget.png" />
+    <script src="https://kit.fontawesome.com/b3c591f1b4.js" crossorigin="anonymous"></script>
+</head>
+<main>
+<?php
---
+++ partials/nav.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="/?title=targetBox"><i class="fas fa-long-arrow-alt-right"></i></a>
+</nav>
+
+<?php
