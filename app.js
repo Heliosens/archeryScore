ce41061f2f20aef4cd2356f1dd54ce33ab55c9bc
Sylvie-B <unsolo@hotmail.fr>
2022-01-24 16:38:32 +0100

    work on js

--- app.js
+++
@@ -1,6 +0,0 @@
-let fly = [1, 11, 10];
-
-// descending numerical sort
-fly.sort((a, b)=> b - a);
-
-console.log(fly);
---
+++ public/assets/app.js
@@ -0,0 +1,51 @@
+// get main
+let main = document.querySelector('main');
+main.style.height = innerHeight + "px";
+
+// get target
+let targetBox = document.querySelector('.target');
+let target = targetBox.getElementsByTagName('div');
+// shoot = 1 arrow, fly = 3 arrows
+
+let fly = 0; // score for a fly
+let currentScore = 0;   // 0 to total
+
+let oneFly = [];    // oneFly.push(shoot value)
+let shoot = 0;      // length of oneFly = current number of shoot
+let result = [];    // final array      .length = current number of fly
+
+// user click on 3 value = display + ask validation
+for(let i = 0 ; i < target.length ; i++){
+    target[i].addEventListener('click', function () {
+        switch (shoot) {
+            case 0 :        // first arrow
+                shoot = oneFly.push(i);
+                console.log(oneFly);
+                break;
+            case 1 : // second arrow
+                shoot = oneFly.push(i);
+                console.log(oneFly);
+                break;
+            case 2 : // third arrow
+                oneFly.push(i);
+                console.log(oneFly);
+                // descending numerical sort
+                oneFly.sort((a, b)=> b - a);
+                result.push(oneFly);
+
+                // todo display current result in modal window
+                // ask user validation or return = delete current entry
+
+                shoot = 0;
+                oneFly = [];
+                console.log(oneFly);
+
+                // todo for last shoot display last fly
+                // todo if user validate load array
+
+                break;
+        }
+    })
+}
+
+// todo operate eleven to ten plus
